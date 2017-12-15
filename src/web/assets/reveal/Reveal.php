403b84b7edc12b2da4451ff1286365713b46114f
Nate Iler <nate@flipboxdigital.com>
2017-12-15 12:20:39 -0700

    moving ‘Reveal’ from external package

---
+++ src/web/assets/reveal/Reveal.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @copyright  Copyright (c) Flipbox Digital Limited
+ * @license    https://github.com/flipboxfactory/craft-ember/blob/master/LICENSE
+ * @link       https://github.com/flipboxfactory/craft-ember
+ */
+
+namespace flipbox\ember\web\assets\reveal;
+
+use craft\web\AssetBundle;
+use craft\web\assets\cp\CpAsset;
+
+/**
+ * @author Flipbox Factory <hello@flipboxfactory.com>
+ * @since 1.0.0
+ */
+class Reveal extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $sourcePath = __DIR__ . '/dist';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->js = [
+            'Reveal'.$this->dotJs()
+        ];
+
+        parent::init();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public $depends = [
+        CpAsset::class,
+    ];
+}
