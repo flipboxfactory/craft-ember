71e1c13f15e726da965b1d43bbe70d99acbc34af
nateiler <nate@flipboxdigital.com>
2019-01-24 13:10:10 -0700

    Deprecating `QueryHelper::parseBaseParam` and all supporting methods in favor of new `QueryHelper::prepareParam`

--- src/helpers/QueryHelper.php
+++ src/helpers/QueryHelper.php
@@ -71,11 +71,118 @@ class QueryHelper
     }
 
     /**
+     * Prepares a query params input value to be used as a condition.  This will attempt to resolve an object
+     * or look one up based on a 'handle' or other unique string identifier (via the lookup callable).
+     *
+     * ```php
+     *
+     * [
+     *      'and',
+     *      'not' => [
+     *          4, elementFiveHandle, $someElementWithIdOfSix
+     *      ]
+     * ]
+     * ```
+     * Would result in a query condition as `NOT IN (4, 5, 6)`.
+     *
+     * @param $value
+     * @param callable|null $lookup
+     * @return array|string
+     */
+    public static function prepareParam($value, callable $lookup = null)
+    {
+        if (!is_array($value)) {
+
+            // An object (model, element, record)
+            if (is_object($value)) {
+                // Try to grab the Id from it
+                try {
+                    return $value->id;
+                } catch (\Exception $e) {
+                    // Carry on
+                }
+            }
+
+            // alpha identifier (handle, etc)
+            if (is_string($value) &&
+                !in_array($value, ['and', 'or'], true) &&
+                StringHelper::isAlpha($value)
+            ) {
+
+                if (null !== $lookup) {
+                    if (null !== ($val = call_user_func($lookup, $value))) {
+                        return static::prepareParam($val, $lookup);
+                    }
+                }
+            }
+
+            return $value;
+        }
+
+        // Traverse
+        $return = [];
+        foreach ($value as $key => $val) {
+            $return = ArrayHelper::merge(
+                $return,
+                static::prepareParamValue($key, $val, $lookup)
+            );
+        }
+
+        return $return;
+    }
+
+    /**
+     * @param $key
+     * @param $value
+     * @param callable|null $lookup
+     * @return array
+     */
+    protected static function prepareParamValue($key, $value, callable $lookup = null): array
+    {
+        $value = static::prepareParam($value, $lookup);
+
+        // Move arrays up one level
+        if (is_array($value)) {
+            $values = [];
+
+            $firstVal = strtolower(reset($value));
+
+            foreach ($value as $k => $val) {
+                $values = ArrayHelper::merge(
+                    $values,
+                    static::prepareParamValue(
+                        is_numeric($k) ? $key : $k,
+                        $val,
+                        $lookup
+                    )
+                );
+            }
+
+            if (in_array($firstVal, ['and', 'or'], true)) {
+                return [$values];
+            }
+
+            return $values;
+        }
+
+        if (!is_numeric($key)) {
+            if (is_string($value) || is_numeric($value)) {
+                $value = $key . ' ' . $value;
+            }
+        }
+
+        return [$value];
+    }
+
+
+    /**
      * Standard param parsing.
      *
      * @param $value
      * @param $join
      * @return bool
+     *
+     * @deprecated
      */
     public static function parseBaseParam(&$value, &$join): bool
     {
@@ -103,6 +210,8 @@ class QueryHelper
      * @param $value
      * @param $operator
      * @return bool
+     *
+     * @deprecated
      */
     public static function findParamValue(&$value, &$operator): bool
     {
@@ -137,6 +246,8 @@ class QueryHelper
      * @param $operator
      * @param string|int|mixed $defaultValue
      * @return array|string
+     *
+     * @deprecated
      */
     public static function assembleParamValue($value, $operator, $defaultValue = ':default:')
     {
@@ -162,6 +273,8 @@ class QueryHelper
      * @param $value
      * @param $operator
      * @return bool
+     *
+     * @deprecated
      */
     public static function prepParamValue(&$value, &$operator): bool
     {
@@ -193,6 +306,8 @@ class QueryHelper
      * @param $value
      * @param string $default
      * @return mixed|string
+     *
+     * @deprecated
      */
     private static function getJoinType(&$value, $default = 'or')
     {
@@ -217,6 +332,8 @@ class QueryHelper
      * @param $value
      * @param null $operator
      * @return mixed|string
+     *
+     * @deprecated
      */
     private static function findIdFromObjectArray($value, $operator = null)
     {
@@ -233,6 +350,8 @@ class QueryHelper
      * @param $value
      * @param null $operator
      * @return string|array
+     *
+     * @deprecated
      */
     private static function prependOperator($value, $operator = null)
     {
@@ -262,6 +381,8 @@ class QueryHelper
      * Normalizes “empty” values.
      *
      * @param string &$value The param value.
+     *
+     * @deprecated
      */
     private static function normalizeEmptyValue(&$value)
     {
@@ -280,6 +401,8 @@ class QueryHelper
      * @param string &$value Te param value.
      *
      * @return string The operator.
+     *
+     * @deprecated
      */
     private static function parseParamOperator(&$value)
     {
@@ -288,10 +411,10 @@ class QueryHelper
             $operatorLength = strlen($testOperator);
 
             if (strncmp(
-                StringHelper::toLowerCase($value),
-                $testOperator,
-                $operatorLength
-            ) == 0
+                    StringHelper::toLowerCase($value),
+                    $testOperator,
+                    $operatorLength
+                ) == 0
             ) {
                 $value = mb_substr($value, $operatorLength);
 
